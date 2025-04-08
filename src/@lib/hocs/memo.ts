ada4c6d72189119750515ac4ee15531dc8ea7d8c
sungchan.kim <sungchan.kim@datastreams.co.kr>
2025-04-08 16:52:02 +0900

    Chore: 코드 컨벤션

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
 import { ComponentType, createElement } from "react";
 import { useRef } from "../hooks";
@@ -10,16 +9,16 @@ export function memo<P extends object>(
   // 2. 메모이제이션된 컴포넌트 생성
   return (props: P) => {
     // 1. 이전 props를 저장할 ref 생성
-    let prevProps = useRef({})
-    let prevComponent = useRef({})
+    const prevProps = useRef({});
+    const prevComponent = useRef({});
     // 3. equals 함수를 사용하여 props 비교
-    if(prevProps.current && _equals(prevProps.current, props)) {
-      return prevComponent.current
+    if (prevProps.current && _equals(prevProps.current, props)) {
+      return prevComponent.current;
     }
     // 4. props가 변경된 경우에만 새로운 렌더링 수행
-    const newElement = createElement(Component, props)
-    prevProps.current = props
-    prevComponent.current = newElement
-    return newElement
-  }
+    const newElement = createElement(Component, props);
+    prevProps.current = props;
+    prevComponent.current = newElement;
+    return newElement;
+  };
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
 import { useRef } from "./useRef";
@@ -9,15 +8,15 @@ export function useMemo<T>(
   _equals = shallowEquals,
 ): T {
   // 1. 이전 의존성과 결과를 저장할 ref 생성
-  let prevDeps = useRef<DependencyList>([])
-  let prevResult = useRef<T | undefined>(undefined)
+  const prevDeps = useRef<DependencyList>([]);
+  const prevResult = useRef<T | undefined>(undefined);
   // 2. 현재 의존성과 이전 의존성 비교
-  if(_deps && _equals(_deps, prevDeps.current)) {
-    return prevResult.current as T
+  if (_deps && _equals(_deps, prevDeps.current)) {
+    return prevResult.current as T;
   }
   // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-  prevResult.current = factory()
+  prevResult.current = factory();
   prevDeps.current = _deps;
   // 4. 메모이제이션된 값 반환
-  return prevResult.current
+  return prevResult.current;
 }
