c043846587d0cba171abe4e46628d54c276ca42f
maphnew <zcm3118@gmail.com>
2025-04-10 22:02:14 +0900

    Chore: 코드 컨벤션

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -2,9 +2,9 @@ import { User } from "../types";
 import { createContext } from "react";
 
 export interface Auth {
-    user: User | null;
-    login: (email: string, password: string) => void;
-    logout: () => void;
+  user: User | null;
+  login: (email: string, password: string) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext<Auth | undefined>(undefined);
